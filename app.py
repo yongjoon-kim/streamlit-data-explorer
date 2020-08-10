a915683470f41b4e3410b5105609ae33f5e8f0f2
yongjoon kim <yongjoon_kim@DESKTOP-3CM1V4H.attlocal.net>
2020-08-09 17:13:03 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber Pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading Data...')
+data = load_data(100)
+data_load_state.text('Done! (using st.cache)')
